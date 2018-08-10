e359eef90966c005e84fe59d142585bda033b531
Justin <you@example.com>
2018-08-10 12:48:10 +0200

    taster zum zurücksetzen der ausgabe

--- main.c
+++ main.c
@@ -14,29 +14,38 @@ volatile uint8_t h=0;
 ISR(TIMER0_OVF_vect)
 {
 	static uint8_t ISR_zaehler=0;
+	static uint8_t ISR_ms_100=0;
+	static uint8_t ISR_sek=0;
+	static uint8_t ISR_min=0;
+	static uint8_t ISR_h=0;
+
 	
 	TCNT0 = 0;
 	ISR_zaehler++;//zähler hochrechnen
 	if(ISR_zaehler==12)
 	{
-		ms_100++; //milisekunden hochrechnen
+		ISR_ms_100++; //milisekunden hochrechnen
 		
-		if(ms_100==10)
+		
+		if(ISR_ms_100==10)
 		{
 			sek++; //sekunden hochrechnen
-			ms_100=0; //milisekunden auf 0 setzen
+			ISR_sek++;
+			ISR_ms_100=0; //milisekunden auf 0 setzen
 		}
 		
-		if(sek==60)
+		if(ISR_sek==60)
 		{
 			min++; //minuten hochrechnen
-			sek=0; //sekunden auf 0 setzen
+			ISR_min++;
+			ISR_sek=0; //sekunden auf 0 setzen
 		}
 		
-		if(min==60)
+		if(ISR_min==60)
 		{
 			h++; //stunden hochrechnen
-			min=0; //minuten auf 0 setzen
+			ISR_h++;
+			ISR_min=0; //minuten auf 0 setzen
 		}
 		
 		ISR_zaehler=0; //zähler auf 0 setzen
@@ -47,20 +56,30 @@ ISR(TIMER0_OVF_vect)
 
 int main(void)
 {
+	
+	uint8_t licht=0;
 	DDRB=0xFF; //Ganzer Port B als Ausgang 
 	PORTB=0x00; //Alle LEDs ausschalten
 
-	TCCR0A		= 0x00;
-	TCCR0B		= 0x04;
-	TIMSK0		|= (1 << TOIE0);
-	TIFR0	|= (1 << TOV0);
+	TCCR0A		= 0x00; 			//Timer Konfigurieren
+	TCCR0B		= 0x04; 			//Timer Konfigurieren
+	TIMSK0		|= (1 << TOIE0);	//Timer Konfigurieren
+	TIFR0	|= (1 << TOV0);			//Timer Konfigurieren
 	sei();
 	
 	
 	while(1)
 	{
+	
+		if((!(PINC&(1<<PC4)))||(!(PINC&(1<<PC5)))) //taster 4 und 5 abfragen
+		{
+			sek=0;
+			min=0;
+			h=0;
+		}
 		
 		PORTB=min; //ausgabe der minuten auf Port B
+		
 	}
 	
 } //end of main
